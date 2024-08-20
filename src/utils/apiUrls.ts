dcad69f207ca197e61e817f94bdca35feec7325d
NullPointError07 <md.taukir.alam@g.bracu.ac.bd>
2024-08-20 12:39:31 +0600

    added explicit types

--- src/utils/apiUrls.ts
+++ src/utils/apiUrls.ts
@@ -2,6 +2,7 @@ export const genderDetectionApi: string =
   process.env.GENDER_DETECTION_API_URL ?? "";
 export const genderPublicationApi: string =
   process.env.GD_PUBLICATION_API_URL ?? "";
-export const queueAdjustmentApi = process.env.QUEUE_ADJUSTMENT_API_URL ?? "";
-export const updateUnqueudPresentsApi =
+export const queueAdjustmentApi: string =
+  process.env.QUEUE_ADJUSTMENT_API_URL ?? "";
+export const updateUnqueudPresentsApi: string =
   process.env.QUEUE_ADJUSTMENT_ACKNOWLEDGEMENT_API_URL ?? "";
