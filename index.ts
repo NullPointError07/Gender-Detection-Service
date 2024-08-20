1b1b94470f171a235eab1d9cb72941682899ef55
NullPointError07 <md.taukir.alam@g.bracu.ac.bd>
2024-08-20 11:35:31 +0600

    removed unnecessary comments

--- index.ts
+++ index.ts
@@ -21,7 +21,6 @@ app.get("/", (req, res) => {
 app.use("/gd-set-queue", GdSetQueueRouter);
 app.use("/gd-queue-processor", GdQueueProcessorRouter);
 
-
 // define gd-prcessor-job
 const queueProcessorJob = CronJob.from({
   cronTime: "*/15 * * * *", // means it will run everyday at every 5 mins
@@ -44,7 +43,6 @@ app.get("/stop-gd-processor", (req: Request, res: Response) => {
   console.log("gd-processor stopped");
   res.status(200).json({ message: "gd-processor stopped successfully" });
 });
-
 
 // define gd-publisher-job
 const publisherJob = CronJob.from({
@@ -69,7 +67,6 @@ app.get("/stop-gd-publisher", (req: Request, res: Response) => {
   res.status(200).json({ message: "gd-publisher stopped successfully" });
 });
 
-
 // define queue-adjustment-job
 const queueAdjustmentJob = CronJob.from({
   cronTime: "* * * * *", // means it will run everyday at every 6 a.m
@@ -92,60 +89,6 @@ app.get("/stop-queue-adjustment", (req: Request, res: Response) => {
   console.log("queue-adjustment stopped");
   res.status(200).json({ message: "queue-adjustment stopped successfully" });
 });
-
-// app.get("/mock", (req, res) => {
-//   cronOutputPublisher();
-// });
-
-// this is just demo, it will be cron
-// app.get("/queue-adjustment", (req, res) => {
-//   cronQueueAdjustment();
-// });
-
-// app.post("/mock-ai", (req, res) => {
-//   console.log("Received data:", req.body);
-
-//   setTimeout(() => {
-//     res
-//       .status(200)
-//       .json({ status: 0, error_type: "timeout", detail: "process timeout" });
-//   }, 3000);
-// });
-
-// app.post("/mock-ai", (req, res) => {
-//   console.log("Received data:", req.body);
-
-//   setTimeout(() => {
-//     res.status(200).json({
-//       status: 1,
-//       data: {
-//         max_person_count: 2,
-//         male_percentage: 55.56,
-//         female_percentage: 44.44,
-//         total_frame_count: 427,
-//         video_duration: "0h 1m 25s",
-//       },
-//     });
-//   }, 5000);
-// });
-
-// app.post("/mock-fanfare", (req, res) => {
-//   console.log("Received data:", req.body);
-
-//   setTimeout(() => {
-//     res.status(200).json({ status: 1, msg: "gd publish success" });
-//   }, 3000);
-// });
-
-// app.post("/mock-fanfare", (req, res) => {
-//   console.log("Received data:", req.body);
-
-//   setTimeout(() => {
-//     res
-//       .status(200)
-//       .json({ status: 0, error_type: "timeout", msg: "random fail message" });
-//   }, 3000);
-// });
 
 app.listen(port, () => {
   console.log(`Server has started on port http://localhost:${port}/`);
